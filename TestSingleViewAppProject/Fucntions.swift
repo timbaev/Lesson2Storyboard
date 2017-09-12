90b1da69f99a785662e30642e7c2254532cbf660
Ildar Zalyalov <hnsworld@yandex.ru>
2017-09-12 11:19:20 +0300

    Lesson project

---
+++ TestSingleViewAppProject/Fucntions.swift
@@ -0,0 +1,10 @@
+//
+//  Fucntions.swift
+//  TestSingleViewAppProject
+//
+//  Created by Ildar Zalyalov on 11.09.17.
+//  Copyright © 2017 ru.itisIosLab. All rights reserved.
+//
+
+import Foundation
+
