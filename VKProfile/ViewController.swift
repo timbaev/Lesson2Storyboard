1980d754c5498830970ac13240b4dbc9ff088561
Тимур Шафигуллин <timursafigullin@Timurs-Mac.local>
2017-09-18 02:02:08 +0300

    improved code, fixed storyboard

--- VKProfile/ViewController.swift
+++ VKProfile/ViewController.swift
@@ -8,25 +8,25 @@
 
 import UIKit
 
+fileprivate enum BorderPosition {
+    case top
+    case bottom
+    case right
+    case left
+}
+
+fileprivate enum InfoType: String {
+    case friends = "друг"
+    case followers = "подпичсик"
+    case groups = "группа"
+    case photos = "фото"
+    case videos = "видео"
+    case audios = "аудио"
+    case presents = "подарок"
+    case files = "файл"
+}
+
 class ViewController: UIViewController {
-    
-    enum BorderPosition {
-        case top
-        case bottom
-        case right
-        case left
-    }
-    
-    enum InfoType {
-        case friends
-        case followers
-        case groups
-        case photos
-        case videos
-        case audios
-        case presents
-        case files
-    }
 
     @IBOutlet weak var infoScrollView: UIScrollView!
     @IBOutlet weak var photoScrollView: UIScrollView!
@@ -41,55 +41,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var photosButton: UIButton!
     @IBOutlet var photos: [UIImageView]!
     
+    var count = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         createStyles()
-        let user = UserInfoData.generateUser()
-        
-        self.title = user.name
-        nameLabel.text = user.name + " " + user.surname
-        onlineStatusLabel.text = user.isOnline ? "online" : "offline"
-        ageLabel.text = String(user.age)
-        if (user.age == 1) {
-            yearsLabel.text = "год"
-        } else {
-            yearsLabel.text = user.age > 4 ? "лет" : "года"
-        }
-        yearsLabel.text = yearsLabel.text! + ","
-        cityLabel.text = user.city
-        
-        let paragraph = NSMutableParagraphStyle()
-        paragraph.alignment = .center
-        let attributes: [String: Any] = [NSParagraphStyleAttributeName: paragraph]
-        setTitle(with: infoButtons[0], type: .friends, count: user.friends, attributes: attributes)
-        setTitle(with: infoButtons[1], type: .followers, count: user.followers, attributes: attributes)
-        setTitle(with: infoButtons[2], type: .groups, count: user.groups, attributes: attributes)
-        setTitle(with: infoButtons[3], type: .photos, count: user.photos.count, attributes: attributes)
-        setTitle(with: infoButtons[4], type: .videos, count: user.videos, attributes: attributes)
-        setTitle(with: infoButtons[5], type: .audios, count: user.audios, attributes: attributes)
-        setTitle(with: infoButtons[6], type: .presents, count: user.presents, attributes: attributes)
-        setTitle(with: infoButtons[7], type: .files, count: user.files, attributes: attributes)
-        
-        photosButton.setTitle("\(user.photos.count) фотографии", for: .normal)
-        for (i, photo) in photos.enumerated() {
-            photo.image = user.photos[i]
-        }
-        avatarImage.image = user.profileImage
+        setLabels()
     }
     
     private func createBorders(to view: UIView, on position: BorderPosition) {
         let borderWidth = CGFloat(2.0)
         let marginX = CGFloat(10)
-        let borderLength = CGFloat(320 - marginX * 2) //hardcode for iPhone SE
+        let borderLength = CGFloat(UIScreen.main.bounds.width - marginX * 2)
         let borderColor = UIColor(rgb: 0xdbd6d6).cgColor
+        let borderMargin: CGFloat = 1.0
+        let noneMargin:CGFloat = 0
         
         switch position {
         case .top:
             let borderTop = CALayer()
             borderTop.borderColor = borderColor
-            borderTop.frame = CGRect(x: marginX, y: 0, width: borderLength, height: 1.5)
+            borderTop.frame = CGRect(x: marginX, y: noneMargin, width: borderLength, height: borderMargin)
             borderTop.borderWidth = borderWidth
             
             view.layer.addSublayer(borderTop)
@@ -98,7 +72,7 @@ class ViewController: UIViewController {
         case .bottom:
             let borderBottom = CALayer()
             borderBottom.borderColor = borderColor
-            borderBottom.frame = CGRect(x: marginX, y: view.frame.height - 1.0, width: borderLength, height: view.frame.height - 1.0)
+            borderBottom.frame = CGRect(x: marginX, y: view.frame.height - borderMargin, width: borderLength, height: view.frame.height - borderMargin)
             borderBottom.borderWidth = borderWidth
             
             view.layer.addSublayer(borderBottom)
@@ -107,7 +81,7 @@ class ViewController: UIViewController {
         case .right:
             let borderRight = CALayer()
             borderRight.borderColor = borderColor
-            borderRight.frame = CGRect(x: view.frame.width - 1.0, y: 0, width: 1, height: view.frame.height)
+            borderRight.frame = CGRect(x: view.frame.width - borderMargin, y: noneMargin, width: borderMargin, height: view.frame.height)
             borderRight.borderWidth = borderWidth
             
             view.layer.addSublayer(borderRight)
@@ -115,7 +89,7 @@ class ViewController: UIViewController {
         case .left:
             let borderLeft = CALayer()
             borderLeft.borderColor = borderColor
-            borderLeft.frame = CGRect(x: 0, y: 0, width: 1, height: view.frame.height)
+            borderLeft.frame = CGRect(x: noneMargin, y: noneMargin, width: borderMargin, height: view.frame.height)
             
             view.layer.addSublayer(borderLeft)
             break
@@ -138,10 +112,47 @@ class ViewController: UIViewController {
         self.view.layoutIfNeeded()
     }
     
-    var count = 0
+    private func setLabels() {
+        let online = "Online"
+        let offline = "Offlien"
+        let year = "год"
+        let years = "лет"
+        let seperator = ","
+        let photosText = "фотографии"
+        
+        let user = UserInfoData.generateUser()
+        self.title = user.name
+        nameLabel.text = user.name + " " + user.surname
+        onlineStatusLabel.text = user.isOnline ? online : offline
+        ageLabel.text = String(user.age)
+        if (user.age == 1) {
+            yearsLabel.text = year
+        } else {
+            yearsLabel.text = user.age > 4 ? years : year + "а"
+        }
+        yearsLabel.text = yearsLabel.text! + seperator
+        cityLabel.text = user.city
+        
+        let paragraph = NSMutableParagraphStyle()
+        paragraph.alignment = .center
+        let attributes: [String: Any] = [NSParagraphStyleAttributeName: paragraph]
+        setTitle(with: infoButtons[0], type: .friends, count: user.friends, attributes: attributes)
+        setTitle(with: infoButtons[1], type: .followers, count: user.followers, attributes: attributes)
+        setTitle(with: infoButtons[2], type: .groups, count: user.groups, attributes: attributes)
+        setTitle(with: infoButtons[3], type: .photos, count: user.photos.count, attributes: attributes)
+        setTitle(with: infoButtons[4], type: .videos, count: user.videos, attributes: attributes)
+        setTitle(with: infoButtons[5], type: .audios, count: user.audios, attributes: attributes)
+        setTitle(with: infoButtons[6], type: .presents, count: user.presents, attributes: attributes)
+        setTitle(with: infoButtons[7], type: .files, count: user.files, attributes: attributes)
+        
+        photosButton.setTitle("\(user.photos.count) " + photosText, for: .normal)
+        photos.enumerated().forEach{ $0.element.image = user.photos[$0.offset] }
+        avatarImage.image = user.profileImage
+    }
+    
     @IBAction func onInfoClick(_ sender: UIButton) {
         if (count == 5) {
-            viewDidLoad()
+            setLabels()
             count = 0
         } else {
             count += 1
@@ -149,60 +160,51 @@ class ViewController: UIViewController {
     }
     
     private func setTitle(with button: UIButton, type: InfoType, count: Int, attributes: [String : Any]) {
-        var title: String
-//        let moreThanFore = count % 10 > 4
-//        let notOne = count % 10 != 1
+        var title = type.rawValue
+        let friends = "друзей"
+        let group = "групп"
+        let groups = "группы"
+        let endingA = "а"
+        let endingOV = "ов"
         
         switch type {
         case .friends:
-            title = "друг"
             if (count % 10 > 4) {
-                title = "друзей"
+                title = friends
             } else if (count % 10 != 1) {
-                title = "друга"
+                title += endingA
             }
             break
         case .followers:
-            title = "подписчик"
             if (count % 10 > 4) {
-                title = "подписчиков"
+                title += endingOV
             } else if (count % 10 != 1) {
-                title = "подписчика"
+                title += endingA
             }
             break
         case .groups:
-            title = "группа"
             if (count % 10 > 4) {
-                title = "групп"
+                title = group
             }
             else if (count % 10 != 1) {
-                title = "группы"
+                title = groups
             }
             break
-        case .photos:
-            title = "фото"
-            break
-        case .videos:
-            title = "видео"
-            break
-        case .audios:
-            title = "аудио"
-            break
         case .presents:
-            title = "подарок"
             if (count % 10 > 4) {
-                title = "подарков"
+                title += endingOV
             } else if (count % 10 != 1) {
-                title = "подарка"
+                title += endingA
             }
             break
         case .files:
-            title = "файл"
             if (count % 10 > 4) {
-                title = "файлов"
+                title += endingOV
             } else if (count % 10 != 1) {
-                title = "файла"
+                title += endingA
             }
+            break
+        default:
             break
         }
         
